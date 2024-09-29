8d913051c1aaa30111211181d68b62af96e8a940
Gustavo <gustavobarsan@gmail.com>
2024-09-29 00:01:19 -0300

    fix
    no ";"

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import * as grpc from '@grpc/grpc-js';
 import * as protoLoader from '@grpc/proto-loader';
 import { Server as GrpcServer, ServerCredentials } from '@grpc/grpc-js';
-import path from 'path';
+import path from 'path'
 
-const PROTO_PATH = path.resolve(__dirname, '../protos/pubsub.proto');
+const PROTO_PATH = path.resolve(__dirname, '../protos/pubsub.proto')
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
     keepCase: true,
